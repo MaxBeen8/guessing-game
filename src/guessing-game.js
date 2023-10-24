0b7930bb6867d0448440f52e7e63df503031401f
Maksim <maximkondratyuk@gmail.com>
2023-10-24 15:38:31 +0300

    feat: add answer

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+      return Math.round((this.right + this.left) / 2);
     }
 
     lower() {
-
+        return this.right = Math.round((this.right + this.left) / 2);
     }
 
     greater() {
-
+        return this.left = Math.round((this.right + this.left) / 2);
     }
 }
 
